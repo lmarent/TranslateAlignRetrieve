d6c2ddd8859e32305fe144059c99d4cbc01c4152
Luis Andres Marentes Cubillos <l_marent@yahoo.com>
2021-02-15 08:19:13 -0500

    Changes for average model to put correct files

--- src/tar/src/nmt/average_models.sh
+++ src/tar/src/nmt/average_models.sh
@@ -12,16 +12,16 @@ LANG_TGT=$5
 
 if [[ $# -eq 0 ]]
   then
-  MODEL1=${SCRIPT_DIR}/data/en2es/train/shared/en2es_transformer_shared_vocab_embs_step_180000.pt
-  MODEL2=${SCRIPT_DIR}/data/en2es/train/shared/en2es_transformer_shared_vocab_embs_step_190000.pt
-  MODEL3=${SCRIPT_DIR}/data/en2es/train/shared/en2es_transformer_shared_vocab_embs_step_200000.pt
+  MODEL1=/content/gdrive/MyDrive/data/exp/model.en-es_step_100000.pt
+  MODEL2=/content/gdrive/MyDrive/data/exp/model.en-es_step_99000.pt
+  MODEL3=/content/gdrive/MyDrive/data/exp/model.en-es_step_98000.pt
   LANG_SRC=en
   LANG_TGT=es
 fi
 
 echo -e "Averaging models...\n${MODEL1}\n${MODEL2}\n${MODEL3}"
 
-MODEL_DIR=$(dirname ${MODEL1})
+MODEL_DIR=/content/gdrive/MyDrive/data/exp/
 AVERAGE_MODEL=${MODEL_DIR}/${LANG_SRC}"2"${LANG_TGT}_average_model.pt
 
 python ${SCRIPT_DIR}/tools/OpenNMT-py/tools/average_models.py \
