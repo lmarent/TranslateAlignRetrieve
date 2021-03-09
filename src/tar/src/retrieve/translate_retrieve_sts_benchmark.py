b84543c9937e095c1390747cf03577d15775fef5
Luis Andres Marentes Cubillos <l_marent@yahoo.com>
2021-03-08 20:39:35 -0500

    Update translate_retrieve_sts_benchmark.py
    Create STS Translate processor

--- src/tar/src/retrieve/translate_retrieve_sts_benchmark.py
+++ src/tar/src/retrieve/translate_retrieve_sts_benchmark.py
@@ -43,9 +43,9 @@ def tokenize_sentences_unlimited_size(text, lang):
                  for chunk in split_sentences(text, lang, '|', 100000000000)]
     return sentences
 
-class SNLITranslator:
+class STSBenchmarkTranslator:
     def __init__(self,
-                 snli_file,
+                 sts_benchmark_file,
                  lang_source,
                  lang_target,
                  output_dir,
@@ -53,74 +53,64 @@ class SNLITranslator:
                  answers_from_alignment,
                  batch_size):
 
-        self.snli_file = snli_file
+        self.sts_benchmark_file = sts_benchmark_file
         self.lang_source = lang_source
         self.lang_target = lang_target
         self.output_dir = output_dir
         self.alignment_type = alignment_type
-        self.answers_from_alignment = answers_from_alignment
         self.batch_size = batch_size
 
         # initialize content_translations_alignments
         self.content_translations_alignments = defaultdict()
 
         # initialize SNLI version
-        self.snli_version = '1.0'
+        self.sts_benchmark_version = '2017'
 
-    # Translate all the textual content in the SNLI dataset,
-    # that are, sentences and gold classification.
-    # The alignment between context and its translation is then computed.
-    # The output is a dictionary with sentence pairs, sentences and classification
-    # and their translation/alignment as values
     def translate_align_content(self):
+        # Translate all the textual content in the SNLI dataset,
+        # that are, sentences and gold classification.
+        # The alignment between context and its translation is then computed.
+        # The output is a dictionary with sentence pairs, sentences and classification
+        # and their translation/alignment as values
 
         stanza.download('es', processors='tokenize,mwt,pos,lemma,depparse')
         nlp = stanza.Pipeline('es', processors='tokenize,mwt,pos,lemma,depparse')
 
         # Load snli content and get snli contexts
-        with open(self.snli_file) as hn:
-            lines = hn.readlines()
+        headers = ['genre' , 'filename', 'year', 'captionID', 'score', 'sentence1', 'sentence2']
         content_lines = []
-        for line in lines:
-            content_lines.append(json.loads(line))
+        with open(self.sts_benchmark_file) as hn:
+            csv = csv.reader(hn, delimiter='\t')
+            for row in csvFile:
+                line = {}
+                for i in range(len(headers)): 
+                    line[header[i]] = row[i]
+                content_lines.append(line)
 
-        # Check if the content of SNLI has been translated and aligned already
+        # Check if the content of STS Benchmark has been translated and aligned already
         content_translations_alignments_file = os.path.join(self.output_dir,
                                                     '{}_content_translations_alignments.{}'.format(
                                                         os.path.basename(self.snli_file),
                                                         self.lang_target))
         if not os.path.isfile(content_translations_alignments_file):
-            # Extract contexts, questions and answers. The context is further
-            # divided into sentence in order to translate and compute the alignment.
+            # Extract sentence one and two.             
             sentences_one = []
             sentences_two = []
-            sentences_one_parse = []
-            sentences_two_parse = []
-            sentences_one_binary_parse = []
-            sentences_two_binary_parse = []
             max_len_sentence_1 = 0
             max_len_sentence_2 = 0
             for content in tqdm(content_lines):
-                if len(content['sentence1']) > max_len_sentence_1:
-                    max_len_sentence_1 = len(content['sentence1'])
-                sentences_one.extend(tokenize_sentences(content['sentence1'],
+                if len(content[5]) > max_len_sentence_1:
+                    max_len_sentence_1 = len(content[5])
+                sentences_one.extend(tokenize_sentences(content[5],
                                                               lang=self.lang_source))
 
-                if len(content['sentence2']) > max_len_sentence_2:
-                    max_len_sentence_2 = len(content['sentence2'])
-                sentences_two.extend(tokenize_sentences(content['sentence2'],
+                if len(content[6]) > max_len_sentence_2:
+                    max_len_sentence_2 = len(content[6])
+                sentences_two.extend(tokenize_sentences(content[6],
                                                               lang=self.lang_source))
-                sentences_one_parse.extend(tokenize_sentences_unlimited_size(content['sentence1_parse'],
-                                                                    lang=self.lang_source))
-                sentences_two_parse.extend(tokenize_sentences_unlimited_size(content['sentence2_parse'],
-                                                                    lang=self.lang_source))
-                sentences_one_binary_parse.extend(tokenize_sentences_unlimited_size(content['sentence1_binary_parse'],
-                                                                           lang=self.lang_source))
-                sentences_two_binary_parse.extend(tokenize_sentences_unlimited_size(content['sentence2_binary_parse'],
-                                                                           lang=self.lang_source))
 
-            sentence_one_translated = utils.translate(sentences_one, self.snli_file, self.output_dir, self.batch_size)
-            sentence_two_translated = utils.translate(sentences_two, self.snli_file,
+            sentence_one_translated = utils.translate(sentences_one, self.sts_benchmark_file, self.output_dir, self.batch_size)
+            sentence_two_translated = utils.translate(sentences_two, self.sts_benchmark_file,
                                                       self.output_dir, self.batch_size)
 
             logging.info('Collected {} sentence to translate'.format(len(sentences_one)))
@@ -140,10 +130,11 @@ class SNLITranslator:
 
                     content_line['sentence1_parse'] = sentence_one_parsed.to_dict()
                     content_line['sentence2_parse'] = sentence_two_parsed.to_dict()
-                    content_line['annotator_labels'] = content['annotator_labels']
+                    content_line['genre'] = content['genre']
                     content_line['captionID'] = content['captionID']
-                    content_line['gold_label'] = content['gold_label']
-                    content_line['pairID'] = content['pairID']
+                    content_line['score'] = content['score']
+                    content_line['year'] = content['year']
+                    content_line['filename'] = content['filename']
                     json.dump(content_line, fn)
                     fn.write('\n')
                     i = i + 1
@@ -158,13 +149,11 @@ class SNLITranslator:
 if __name__ == "__main__":
     start = time.time()
     parser = argparse.ArgumentParser()
-    parser.add_argument('-snli_file', type=str, help='SNLI dataset to translate')
+    parser.add_argument('-sts_benchmark_file', type=str, help='STS Benchmark dataset to translate')
     parser.add_argument('-lang_source', type=str, default='en',
-                        help='language of the SNLI dataset to translate (the default value is set to English)')
+                        help='language of the STS Benchmark dataset to translate (the default value is set to English)')
     parser.add_argument('-lang_target', type=str, help='translation language')
     parser.add_argument('-output_dir', type=str, help='directory where all the generated files are stored')
-    parser.add_argument('-answers_from_alignment', action='store_true',
-                        help='retrieve translated answers only from the alignment')
     parser.add_argument('-alignment_type', type=str,
                         default='forward', help='use a given translation service')
     parser.add_argument('-batch_size', type=int, default='32', help='batch_size for the translation script '
@@ -176,15 +165,14 @@ if __name__ == "__main__":
     except FileExistsError:
         pass
 
-    translator = SNLITranslator(args.snli_file,
+    translator = STSBenchmarkTranslator(args.sts_benchmark_file,
                                  args.lang_source,
                                  args.lang_target,
                                  args.output_dir,
                                  args.alignment_type,
-                                 args.answers_from_alignment,
                                  args.batch_size)
 
-    logging.info('Translate SNLI textual content')
+    logging.info('Translate STS Benchmark textual content')
     translator.translate_align_content()
 
 
