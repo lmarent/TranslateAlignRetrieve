77951ed261cfec9d8ffe04da66b1ce7a0087ef8e
Luis Andres Marentes Cubillos <l_marent@yahoo.com>
2021-03-08 21:10:32 -0500

    Update translate_sts_benchmark.sh

--- src/tar/translate_sts_benchmark.sh
+++ src/tar/translate_sts_benchmark.sh
@@ -12,7 +12,7 @@ LANG_TGT=es
 TRANSLATE_RETRIEVE_DIR=${SCRIPT_DIR}/src/retrieve
 
 python ${TRANSLATE_RETRIEVE_DIR}/translate_retrieve_sts_benchmark.py \
-           -sts_benchmark_file   ${STS_FILE} \
+           -sts_benchmark_file ${STS_FILE} \
            -lang_source ${LANG_SRC} \
            -lang_target ${LANG_TGT} \
            -output_dir ${OUTPUT_DIR}
